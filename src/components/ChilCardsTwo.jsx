786e5d30e456d22ef515d0fe103a0bbc66ee64b3
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-09 11:36:38 +0530

    Section Build

--- src/components/ChilCardsTwo.jsx
+++ src/components/ChilCardsTwo.jsx
@@ -6,10 +6,10 @@ function ChilCards() {
     <div className="w-full h-full pb-1 px-16 flex justify-center items-center gap-8">
       <div className="w-1/2 h-full rounded-3xl bg-cover bg-center bg-[#E3EEFC] flex justify-between items-center flex-col overflow-hidden relative">
         <div className="w-full h-1/2 flex flex-col justify-center items-center px-24 -mt-16">
-          <p className="text-zinc-800">Pixel Buds Pro</p>
+          <p className="text-zinc-800 mt-5">Nest Mini (2nd gen)</p>
 
-          <h1 className="text-zinc-700 font-bold text-4xl leading-none tracking-tight py-1 text-center">
-            Premium sound, <br></br>pristine calls.
+          <h1 className="text-zinc-700 font-bold text-4xl leading-none tracking-tight py-4 text-center">
+            Still mini, Even, <br></br>more mighty.
           </h1>
           <button className="mt-6 bg-transparent font-semibold text-zinc-700 border-2 border-zinc-700 px-3 py-1 pb-2 rounded-md tracking-tight hover:bg-zinc-700 hover:text-zinc-200 cursor-pointer">
             Learn more
@@ -25,10 +25,10 @@ function ChilCards() {
 
       <div className="w-1/2 h-full rounded-3xl bg-cover bg-center bg-[#E3EEFC] flex justify-between items-center flex-col overflow-hidden relative">
         <div className="w-full h-1/2 flex flex-col justify-center items-center px-24 -mt-16">
-          <p className="text-zinc-800">Pixel Buds A-series</p>
+          <p className="text-zinc-800 mt-5">Nest Audio</p>
 
-          <h1 className="text-zinc-700 font-bold text-4xl leading-none tracking-tight py-1 text-center">
-            Rich sound, always.
+          <h1 className="text-zinc-700 font-bold text-4xl leading-none tracking-tight py-4 text-center">
+          Amazing sound. At <br></br>your command.
           </h1>
           <button className="mt-6 bg-transparent font-semibold text-zinc-700 border-2 border-zinc-700 px-3 py-1 pb-2 rounded-md tracking-tight hover:bg-zinc-700 hover:text-zinc-200 cursor-pointer">
             Learn more
--- src/components/SecondCards.jsx
+++ src/components/SecondCards.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function FirstCards() {
   return (
     <div className="w-full h-auto py-8 px-16">
-      <div className="w-full h-[88vh] rounded-3xl bg-cover bg-center bg-[#E3EEFC] flex justify-between items-center flex-row-reverse overflow-hidden">
+      <div className="w-full h-[88vh] rounded-3xl bg-cover bg-center bg-[#CEDBDF] flex justify-between items-center flex-row-reverse overflow-hidden">
         <div className="w-1/2 h-full flex flex-col justify-center items-start px-24">
-          <p className="text-zinc-800 my-3">Google Pixel Watch 2</p>
-          {["Help by Google.", "Health by Fitbit.", "Made for you."].map(
+          <p className="text-zinc-800 my-3">Nest Cam (battery)</p>
+          {[" Peace of mind in", "more palces."].map(
             (item, idx) => (
               <h1
                 key={idx}
@@ -17,17 +17,14 @@ function FirstCards() {
             )
           )}
 
-          <p className="text-zinc-800 mt-5">
-            Get personalised help for your health, safety and <br></br>{" "}
-            productivity.
-          </p>
+        
           <button className="mt-6 bg-transparent font-bold text-zinc-700 border-2 border-zinc-700 px-6 py-2 pb-3 rounded-md tracking-tight hover:bg-zinc-700 hover:text-zinc-200 cursor-pointer">
             Learn more
           </button>
         </div>
 
         <div className="w-1/2 h-full relative">
-          <div className='w-full h-[80vh] absolute top-[19%] left-[2%] bg-contain bg-no-repeat bg-[url("https://lh3.googleusercontent.com/m-EKuG8rRZ_CUm0F7TgRiVCEnZ6gcEZAN93Qu_Gun5HDvjmwphPeDYeZ9kkVAEPEjISWFxojQCcUG6D8jkKa2FRB_gDZCwJmW7E=rw-e365-w1200")]'></div>
+          <div className='w-full h-[80vh] absolute top-[1%] left-[2%] bg-cover bg-no-repeat bg-[url("https://github.com/Piyush289kumar/google-store/blob/main/src/assets/images/CemegaPng.png?raw=true")]'></div>
         </div>
 
       </div>
